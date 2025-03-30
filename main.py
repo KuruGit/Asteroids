11ec98c21ed2ba1938a5b4ed3742e9c02f76b9c3
kurugit <david.breier@outlook.de>
2025-03-30 23:24:32 +0200

    basic game loop erstellt

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import pygame
+from constants import *
 
 def main():
-    print ("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    overlay = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        overlay.fill((0,0,0))
+        pygame.display.flip()
 
 
 
-    
+
+
+
 if __name__ == '__main__':
     main()
