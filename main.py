44a2c340299fc92328c0388b46df43f968ab581b
kurugit <david.breier@outlook.de>
2025-04-03 20:39:34 +0200

    leeren Canvas angelegt und Player Sprite angelegt

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     overlay = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+    ticker = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         overlay.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = ticker.tick(60)/1000
 
 
 
